2d039f21d795dd9a4416ced2d3b92f14d5062fd2
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-10 18:29:44 +0100

    init user_refresh_tokens table script

--- src/main/resources/db/migration/v1__initial.sql
+++ src/main/resources/db/migration/v1__initial.sql
@@ -7,17 +7,20 @@ CREATE TABLE users (
     email VARCHAR(320) NOT NULL,
     email_confirmed BOOLEAN NOT NULL DEFAULT FALSE,
     password_hash TEXT NOT NULL
-)
+);
+
+-- TODO email should be unque ignoring case
 
 CREATE UNIQUE INDEX users_email_application_name_unique_idx ON users (email, application_name);
 
+CREATE TABLE user_refresh_tokens (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP NOT NULL DEFAULT now(),
+    expiration TIMESTAMP,
+    state VARCHAR(32) NOT NULL,
+    token_hash TEXT NOT NULL
+    user_id UUID REFERENCES users (id)
+);
 
--- user_refresh_tokens
-----------------------
--- id -> uuid
--- created_date
--- updated_date
--- expiration_date (nullable)
--- state
--- token_hash -> text
--- user_id
+CREATE INDEX user_refresh_tokens_token_hash_idx ON user_refresh_tokens (token_hash);
