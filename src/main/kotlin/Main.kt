88d6a26825955f335ee36c4a40cc76d748e65422
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-10 14:36:32 +0100

    clean up todos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,17 +2,16 @@ import io.javalin.Javalin
 
 
 // TODO
-//  1. Set up mysql database in docker
-//  2. Add flyway support
-//  3. Create auth model similar to django / .net (but with jwt in mind)
-//  4. Add hibernate support
-//  5. Crate sign-up flow
-//      5.1 with jwt
-//      5.2 write tests (unit + e2e with test-containers)
-//  6. Create sign-in flow
-//  7. Create log-out flow
-//  8. TODO App should support 2FA, email confirmation (with resending email confirmation) etc
-//      8.1 Check .net identity and create TODO for functionality that I need to implement
+//  1. Add flyway support
+//  2. Create auth model similar to django / .net (but with jwt in mind)
+//  3. Add hibernate support
+//  4. Crate sign-up flow
+//      4.1 with jwt
+//      4.2 write tests (unit + e2e with test-containers)
+//  5. Create sign-in flow
+//  6. Create log-out flow
+//  7. TODO App should support 2FA, email confirmation (with resending email confirmation) etc
+//      7.1 Check .net identity and create TODO for functionality that I need to implement
 
 fun main() {
     val app = Javalin.create(/*config*/)
