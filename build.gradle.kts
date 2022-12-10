ede825b0996328283a8c157e3807706fbd96ce7a
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-10 21:44:13 +0100

    remove hibernate deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.20"
-    id("org.jetbrains.kotlin.plugin.jpa") version "1.7.21"
     application
 }
 
 group = "app.rutherford"
-version = "1.0-SNAPSHOT"
+version = "0.1"
 
 repositories {
     mavenCentral()
@@ -18,13 +17,8 @@ dependencies {
     implementation("org.slf4j:slf4j-simple:2.0.5")
     implementation("org.flywaydb:flyway-core:9.10.0")
     implementation("org.postgresql:postgresql:42.5.1")
-    implementation("jakarta.el:jakarta.el-api:5.0.1") // for hiber
-    implementation("org.hibernate.orm:hibernate-core:6.1.6.Final")
-    implementation("org.hibernate.orm:hibernate-hikaricp:6.1.5.Final")
-    implementation("org.hibernate.validator:hibernate-validator:8.0.0.Final")
 
     testImplementation(kotlin("test"))
-    testImplementation("org.hibernate.orm:hibernate-testing:6.1.5.Final") // TODO do I need this?
 }
 
 tasks.test {
