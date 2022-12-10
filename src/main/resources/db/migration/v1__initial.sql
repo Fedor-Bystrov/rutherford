58f26db75f0a3d81fe4f0e10bb1d04c9ad0d5148
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-10 18:17:51 +0100

    init users table script

--- src/main/resources/db/migration/v1__initial.sql
+++ src/main/resources/db/migration/v1__initial.sql
@@ -1,15 +1,16 @@
--- TODO create schema
--- users
---------
--- id -> uuid
--- created_date
--- updated_date
--- last_login (date)
--- application_name -> string
--- email
--- email_confirmed -> bool
--- password_hash -> TEXT
---
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP NOT NULL DEFAULT now(),
+    last_login TIMESTAMP,
+    application_name VARCHAR(64) NOT NULL,
+    email VARCHAR(320) NOT NULL,
+    email_confirmed BOOLEAN NOT NULL DEFAULT FALSE,
+    password_hash TEXT NOT NULL
+)
+
+CREATE UNIQUE INDEX users_email_application_name_unique_idx ON users (email, application_name);
+
 
 -- user_refresh_tokens
 ----------------------
