f3d562ae019ce9a6e07e20450a9e34b6ebeeddd3
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-11 23:07:49 +0100

    impl findAll

--- src/main/kotlin/app/rutherford/database/repository/AuthUserRepository.kt
+++ src/main/kotlin/app/rutherford/database/repository/AuthUserRepository.kt
@@ -2,16 +2,18 @@ package app.rutherford.database.repository
 
 import app.rutherford.database.entity.AuthUser
 import app.rutherford.database.entity.AuthUser.Builder.Companion.authUser
+import app.rutherford.database.jooq.generated.tables.daos.AuthUserDao
 import app.rutherford.database.jooq.generated.tables.pojos.JAuthUser
 import app.rutherford.database.jooq.generated.tables.records.AuthUserRecord
 import org.jooq.DSLContext
 
 class AuthUserRepository(private val dslContext: DSLContext) {
+
     fun findAll(): List<AuthUser> {
-        return listOf()
+        return AuthUserDao().findAll().map { fromPojo(it) }
     }
 
-    fun fromPojo(pojo: JAuthUser): AuthUser = authUser()
+    private fun fromPojo(pojo: JAuthUser): AuthUser = authUser()
         .id(pojo.id!!)
         .createdAt(pojo.createdAt!!)
         .updatedAt(pojo.updatedAt!!)
@@ -22,7 +24,7 @@ class AuthUserRepository(private val dslContext: DSLContext) {
         .passwordHash(pojo.passwordHash!!)
         .build()
 
-    fun toPojo(entity: AuthUser): JAuthUser = JAuthUser(
+    private fun toPojo(entity: AuthUser): JAuthUser = JAuthUser(
         id = entity.id,
         createdAt = entity.createdAt,
         updatedAt = entity.updatedAt,
@@ -33,7 +35,7 @@ class AuthUserRepository(private val dslContext: DSLContext) {
         passwordHash = entity.passwordHash,
     )
 
-    fun fromRecord(record: AuthUserRecord): AuthUser = authUser()
+    private fun fromRecord(record: AuthUserRecord): AuthUser = authUser()
         .id(record.id!!)
         .createdAt(record.createdAt!!)
         .updatedAt(record.updatedAt!!)
@@ -44,7 +46,7 @@ class AuthUserRepository(private val dslContext: DSLContext) {
         .passwordHash(record.passwordHash!!)
         .build()
 
-    fun toRecord(entity: AuthUser): AuthUserRecord = AuthUserRecord(
+    private fun toRecord(entity: AuthUser): AuthUserRecord = AuthUserRecord(
         id = entity.id,
         createdAt = entity.createdAt,
         updatedAt = entity.updatedAt,
