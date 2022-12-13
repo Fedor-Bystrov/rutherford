5d7eee07ae4c963fdb95bb24b0c928cdcd05bcf9
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-13 16:54:28 +0100

    improve todos

--- src/main/kotlin/app/rutherford/Main.kt
+++ src/main/kotlin/app/rutherford/Main.kt
@@ -6,9 +6,10 @@ import app.rutherford.module.ApplicationModule
 //  1. Crate sign-up flow
 //      2.1 with jwt
 //      2.2 write tests (unit + e2e with test-containers)
-//  2. Create sign-in flow
-//  3. Create log-out flow
-//  4. TODO App should support 2FA, email confirmation (with resending email confirmation) etc
+//  2. Add exception mapping
+//  3. Create sign-in flow
+//  4. Create log-out flow
+//  5. TODO App should support 2FA, email confirmation (with resending email confirmation) etc
 //      4.1 Check .net identity and create TODO for functionality that I need to implement
 
 // TODO Set up !!
