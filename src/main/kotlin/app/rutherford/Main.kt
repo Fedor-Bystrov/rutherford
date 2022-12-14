09b6ce942ade726ab7b59b27d0582a802542fc85
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-14 20:50:20 +0100

    clean up todos

--- src/main/kotlin/app/rutherford/Main.kt
+++ src/main/kotlin/app/rutherford/Main.kt
@@ -2,7 +2,7 @@ package app.rutherford
 
 import app.rutherford.module.ApplicationModule
 
-// TODO auth functionality
+// TODO Auth Functionality
 //  1. Crate sign-up flow
 //      2.1 with jwt
 //      2.2 write tests (unit + e2e with test-containers)
@@ -12,14 +12,13 @@ import app.rutherford.module.ApplicationModule
 //  5. TODO App should support 2FA, email confirmation (with resending email confirmation) etc
 //      4.1 Check .net identity and create TODO for functionality that I need to implement
 
-// TODO Set up !!
-//  1. Add tests
-//  2. Add logback.xml (similar to penn)
+// TODO Set up
+//  1. Extract JavalinModule
+//  2. Add .env support
 //  3. Set up test containers for e2e tests
-//  4. Fix all other todos
-
-// TODO fix
-//     1. Write tests on repositories
+//  4. Write tests on repositories
+//  5. Add Javalin exception mapping (take teller / penn as example)
+//  5. Fix all other todos
 
 fun main() {
     System.setProperty("org.jooq.no-logo", "true")
