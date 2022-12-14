e5f1b55dc684b58adc27d4004d88861a138c4d07
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-14 23:19:49 +0100

    init ChecksTest

---
+++ src/test/kotlin/app/rutherford/util/ChecksTest.kt
@@ -0,0 +1,26 @@
+package app.rutherford.util
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class ChecksTest {
+    @Test
+    fun `should validate not null`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `should throw if value is null`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `should validate not blank`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `should throw if value is blank or null`() {
+        TODO("Not yet implemented")
+    }
+}
