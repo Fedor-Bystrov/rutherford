e7407316c9c8434eec0ff99e51daa87d0dc734bd
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-15 21:30:12 +0100

    impl should find correct auth_user by id test

--- src/testFunctional/kotlin/app/rutherford/database/repository/AuthUserRepositorySpec.kt
+++ src/testFunctional/kotlin/app/rutherford/database/repository/AuthUserRepositorySpec.kt
@@ -1,25 +1,71 @@
 package app.rutherford.database.repository
 
 import app.rutherford.FunctionalTest
+import app.rutherford.database.entity.AuthUser
 import app.rutherford.database.transaction.transaction
+import app.rutherford.fixtures.anAuthUser
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.*
+import java.util.UUID.randomUUID
+import java.util.stream.Stream
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
 class AuthUserRepositoryFuncTest : FunctionalTest() {
+    companion object {
+        private val userId1 = randomUUID()
+        private val userId2 = randomUUID()
+        private val userId3 = randomUUID()
+
+        @JvmStatic
+        private fun userIds() = Stream.of(arguments(userId1), arguments(userId2), arguments(userId3))
+    }
+
+    private lateinit var user1: AuthUser
+    private lateinit var user2: AuthUser
+    private lateinit var user3: AuthUser
+
     @BeforeEach
     fun setUp() {
+        user1 = anAuthUser().id(userId1).lastLogin(null).build()
+        user2 = anAuthUser().id(userId2).emailConfirmed(true).build()
+        user3 = anAuthUser().id(userId3).build()
+
         transaction {
             authUserRepository.insert(
                 it, listOf(
-
+                    user1,
+                    user2,
+                    user3
                 )
             )
         }
     }
 
-    @Test
-    fun `should find correct auth_user by id`() {
-        TODO("implement")
+    @ParameterizedTest
+    @MethodSource("userIds")
+    fun `should find correct auth_user by id`(userId: UUID) {
+        // given
+        val expected = getExpectedUser(userId)
+
+        // when
+        val result = authUserRepository.find(userId)
+
+        // then
+        assertNotNull(result)
+        assertEquals(expected.id, result.id)
+        assertEquals(expected.createdAt, result.createdAt)
+        assertEquals(expected.updatedAt, result.updatedAt)
+        assertEquals(expected.lastLogin, result.lastLogin)
+        assertEquals(expected.applicationName, result.applicationName)
+        assertEquals(expected.email, result.email)
+        assertEquals(expected.emailConfirmed, result.emailConfirmed)
+        assertEquals(expected.passwordHash, result.passwordHash)
+        assertEquals(expected, result)
     }
 
     @Test
@@ -30,5 +76,12 @@ class AuthUserRepositoryFuncTest : FunctionalTest() {
     @Test
     fun `should get correct auth_user by id`() {
         TODO("implement")
+    }
+
+    private fun getExpectedUser(id: UUID): AuthUser = when (id) {
+        userId1 -> user1
+        userId2 -> user2
+        userId3 -> user3
+        else -> throw IllegalArgumentException("user with id $id not found")
     }
 }
--- src/testFunctional/kotlin/app/rutherford/fixtures/Fixtures.kt
+++ src/testFunctional/kotlin/app/rutherford/fixtures/Fixtures.kt
@@ -1,28 +1,17 @@
 package app.rutherford.fixtures
 
-import app.rutherford.database.entity.AuthUser.Builder
+import app.rutherford.database.entity.AuthUser.Builder.Companion.authUser
 import org.apache.commons.lang3.RandomStringUtils.randomAlphabetic
 import org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric
-import java.time.Instant
 import java.time.Instant.now
-import java.util.*
 import java.util.UUID.randomUUID
 
-fun anAuthUser(
-    id: UUID? = null,
-    createdAt: Instant? = null,
-    updatedAt: Instant? = null,
-    lastLogin: Instant? = null,
-    applicationName: String? = null,
-    email: String? = null,
-    emailConfirmed: Boolean? = null,
-    passwordHash: String? = null,
-) = Builder.authUser()
-    .id(id ?: randomUUID())
-    .createdAt(createdAt ?: now())
-    .updatedAt(updatedAt ?: now())
-    .lastLogin(lastLogin ?: now())
-    .applicationName(applicationName ?: randomAlphanumeric(10))
-    .email(email ?: "${randomAlphabetic(10)}@test.com")
-    .emailConfirmed(emailConfirmed ?: false)
-    .passwordHash(passwordHash ?: randomAlphanumeric(50));
+fun anAuthUser() = authUser()
+    .id(randomUUID())
+    .createdAt(now())
+    .updatedAt(now())
+    .lastLogin(now())
+    .applicationName(randomAlphanumeric(10))
+    .email("${randomAlphabetic(10)}@test.com")
+    .emailConfirmed(false)
+    .passwordHash(randomAlphanumeric(50))
