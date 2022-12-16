7edee0d9c02ec2c249812190cf9ce5fc6ec7ce98
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-16 23:04:07 +0100

    add insert and update to AuthUserTokenRepository

--- src/main/kotlin/app/rutherford/database/repository/AuthUserTokenRepository.kt
+++ src/main/kotlin/app/rutherford/database/repository/AuthUserTokenRepository.kt
@@ -6,6 +6,7 @@ import app.rutherford.database.jooq.generated.tables.records.AuthUserTokenRecord
 import app.rutherford.database.jooq.generated.tables.references.AUTH_USER_TOKEN
 import org.jooq.Configuration
 import org.jooq.DSLContext
+import java.util.*
 
 class AuthUserTokenRepository(
     defaultContext: DSLContext
@@ -15,11 +16,15 @@ class AuthUserTokenRepository(
     AUTH_USER_TOKEN.ID
 ) {
 
+    // TODO what password hashing algo to use? Use the same as in .net (or similar)?
+
     // TODO impl find by user_id and token_hash
     //  - can't just use token_hash since two users can have the same password
     //  - or use user-defined salt to generate token_hash?
     // TODO fix auth_user_token index
+    fun insert(conf: Configuration, entity: AuthUserToken): AuthUserToken = insertOne(conf, entity)
     fun update(conf: Configuration, entity: AuthUserToken): AuthUserToken = updateOne(conf, entity)
+    fun delete(conf: Configuration, id: UUID): Unit = deleteById(conf, id)
 
     override fun fromRecord(record: AuthUserTokenRecord): AuthUserToken = authUserToken()
         .id(record.id!!)
