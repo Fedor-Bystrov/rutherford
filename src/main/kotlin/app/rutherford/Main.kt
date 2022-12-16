07c06be16acca1a2689da305bec3949ac34965b6
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-16 23:16:01 +0100

    clean up Main.kt

--- src/main/kotlin/app/rutherford/Main.kt
+++ src/main/kotlin/app/rutherford/Main.kt
@@ -13,7 +13,6 @@ import app.rutherford.module.ApplicationModule
 //      4.1 Check .net identity and create TODO for functionality that I need to implement
 
 // TODO Set up
-//  - Add AuthUserTokenRepository
 //  - Write test on AuthUserTokenRepository
 //  - Add Javalin exception mapping (take teller / penn as example)
 
