9fc6ecb033669defb1df55f7a11f47deaeaebaa6
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-17 18:58:53 +0100

    add non critical exceptions mapping

--- src/main/kotlin/app/rutherford/module/JavalinModule.kt
+++ src/main/kotlin/app/rutherford/module/JavalinModule.kt
@@ -1,23 +1,34 @@
 package app.rutherford.module
 
+import app.rutherford.database.exception.EntityNotFoundException
+import com.fasterxml.jackson.core.JsonParseException
 import com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES
 import com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.javalin.Javalin
+import io.javalin.http.Context
+import io.javalin.http.ExceptionHandler
+import io.javalin.http.HttpStatus
+import io.javalin.http.HttpStatus.BAD_REQUEST
+import io.javalin.http.HttpStatus.NOT_FOUND
 import io.javalin.json.JavalinJackson
 import io.javalin.validation.JavalinValidation
+import io.javalin.validation.ValidationException
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.util.*
+import kotlin.reflect.KClass
 
 class JavalinModule {
     val javalin: Javalin
+    private val logger: Logger
 
     init {
         // TODO add exception mapper, handle JavalinValidation exception
         JavalinValidation.register(UUID::class.java) { UUID.fromString(it) }
 
-        val logger = LoggerFactory.getLogger("Rutherford")
+        logger = LoggerFactory.getLogger("Rutherford")
         val jsonMapper = JavalinJackson(
             jacksonObjectMapper()
                 .registerModule(JavaTimeModule())
@@ -35,6 +46,11 @@ class JavalinModule {
                 )
             }
         }
+
+        exception(JsonParseException::class) { e, c -> nonCritical(e, c, BAD_REQUEST, "Malformed JSON") }
+        exception(ValidationException::class) { e, c -> nonCritical(e, c, BAD_REQUEST, getMessage(e)) }
+        exception(IllegalStateException::class) { e, c -> nonCritical(e, c, BAD_REQUEST, e.message) }
+        exception(EntityNotFoundException::class) { e, c -> nonCritical(e, c, NOT_FOUND, "Not Found") }
     }
 
     fun start(applicationPort: Int) {
@@ -43,5 +59,23 @@ class JavalinModule {
 
     fun stop() {
         javalin.stop()
+    }
+
+    private fun <E : Exception> exception(clazz: KClass<E>, exceptionHandler: ExceptionHandler<E>) =
+        javalin.exception(clazz.java, exceptionHandler)
+
+    private fun <E : Exception> nonCritical(e: E, ctx: Context, httpStatus: HttpStatus, message: String?) {
+        logger.info("An error occurred with id /*TODO add ID*/", e)
+        ctx.status(httpStatus)
+        ctx.json(
+            mapOf(
+                "message" to message, // TODO swap to fluent json builder
+                "code" to httpStatus.code
+            )
+        )
+    }
+
+    private fun getMessage(e: ValidationException): String {
+        return "Validation error. Invalid parameters: ${e.errors.keys}"
     }
 }
