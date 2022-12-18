a5201cbe360f5d144e36d7888232af248fa28536
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-18 18:20:12 +0100

    clean up todos

--- app/src/main/kotlin/app/rutherford/Main.kt
+++ app/src/main/kotlin/app/rutherford/Main.kt
@@ -13,7 +13,6 @@ import app.rutherford.module.ApplicationModule
 //      4.1 Check .net identity and create TODO for functionality that I need to implement
 
 // TODO Set up
-//  - extract deps to buildSrc Dependencies object
 //  - extract IntegrationTest to core (without javalin)
 //  - Create FunctionalTest in app (with javalin) and http client in future
 
