06eb243b383a19f0a8cb57a8b20214864738ce8e
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-21 12:50:39 +0100

    update todos

--- app/src/main/kotlin/app/rutherford/Main.kt
+++ app/src/main/kotlin/app/rutherford/Main.kt
@@ -2,15 +2,19 @@ package app.rutherford
 
 import app.rutherford.module.ApplicationModule
 
-// TODO Auth Functionality
-//  1. Crate sign-up flow
-//      2.1 with jwt
-//      2.2 write tests (unit + e2e with test-containers)
-//  2. Add exception mapping
-//  3. Create sign-in flow
-//  4. Create log-out flow
-//  5. TODO App should support 2FA, email confirmation (with resending email confirmation) etc
-//      4.1 Check .net identity and create TODO for functionality that I need to implement
+// TODO 1. Basic Auth Functionality
+//  - Create AuthManger it should implement following functionalities
+//      - sign_up (create user)
+//      - sign_in (issue access and refresh tokens given correct user details)
+//      - log_out (remove refresh_token i.e. move refresh_token to deleted state)
+//      - change_password
+//      - anything else (check .net identity)
+//  - Use .net identity as a reference (but with jwt in mind)
+
+// TODO 2. Email confirmation Functionality
+//  - sign_up should send an email to confirm the user's email address
+//  - sign_in should return error and ask to confirm the email
+//      - user should be able to resend confirmation email
 
 fun main() {
     val application = ApplicationModule(Overrides())
