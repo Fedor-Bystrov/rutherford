22b0a2fa00cd7f15d7cd6ca7f802b6b1a91bac97
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-22 14:50:54 +0100

    clean up todos

--- auth/src/main/kotlin/app/rutherford/auth/manager/UserManager.kt
+++ auth/src/main/kotlin/app/rutherford/auth/manager/UserManager.kt
@@ -5,6 +5,17 @@ import app.rutherford.auth.entity.AuthUser.Builder.Companion.authUser
 import app.rutherford.core.ApplicationName
 import app.rutherford.core.util.Checks.validateNotBlank
 
+
+// TODO
+//  - validate password with https://www.passay.org
+//  - validate user given provided email and applicationName combination doesn't exist
+//    - check that necessary index exist in the db
+//  - generate password hash (use the same algo as in .net)
+//  - store user in the database
+//  - return created user
+
+// TODO read https://auth0.com/blog/hashing-passwords-one-way-road-to-security/
+
 class UserManager {
     fun create(
         email: String,
@@ -13,18 +24,6 @@ class UserManager {
     ): AuthUser {
         validateNotBlank("email", email)
         validateNotBlank("password", password)
-
-        // TODO
-        //  - validate password with https://www.passay.org
-        //  - validate user given provided email and applicationName combination doesn't exist
-        //    - check that necessary index exist in the db
-        //  - generate password hash (use the same algo as in .net)
-        //  - store user in the database
-        //  - return created user
-
-        // TODO read https://auth0.com/blog/hashing-passwords-one-way-road-to-security/
-        // TODO use PBKDF2 (as in .net)?
-
         return authUser()
             .build()
     }
