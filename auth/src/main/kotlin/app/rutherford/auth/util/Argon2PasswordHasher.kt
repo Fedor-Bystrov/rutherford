52511a7876be055b93fb76498ff799b21134d2c1
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-23 22:01:27 +0100

    clean up Argon2PasswordHasher

--- auth/src/main/kotlin/app/rutherford/auth/util/Argon2PasswordHasher.kt
+++ auth/src/main/kotlin/app/rutherford/auth/util/Argon2PasswordHasher.kt
@@ -3,42 +3,56 @@ package app.rutherford.auth.util
 import app.rutherford.core.types.Base64
 import org.bouncycastle.crypto.generators.Argon2BytesGenerator
 import org.bouncycastle.crypto.params.Argon2Parameters
+import org.bouncycastle.crypto.params.Argon2Parameters.ARGON2_id
+import java.security.SecureRandom
 
-// TODO write PBKDF2 password generator and compare in runtime
+private const val SALT_SIZE_BYTES = 16
 
-class Argon2PasswordHasher(private val salt: Base64, private val key: Base64) { // TODO test
-    fun hash(password: String): Base64 {
+// Argon parameters
+private const val ARGON_MEMORY_KIB = 15_000 // 15 mb
+private const val ARGON_ITERATIONS = 10
+private const val ARGON_PARALLELISM = 1
+private const val HASH_SIZE_BYTES = 32
+
+data class HashingResult(val salt: Base64, val hash: Base64) {
+    override fun toString(): String = "HashingResult(<masked_data>)"
+}
+
+class Argon2PasswordHasher(encodedSecret: Base64) { // TODO test
+    private val secureRandom = SecureRandom()
+
+    // TODO keep encodedSecret in keystore and extract on each run?
+    private val secret = encodedSecret.decodeBytes()
+
+
+    fun hash(password: String): HashingResult {
+        val salt = nextSalt()
+
         val argonGenerator = Argon2BytesGenerator()
         val argon2Parameters = Argon2Parameters
             // Recommended min by OWASP
-            .Builder(Argon2Parameters.ARGON2_id)
-            .withMemoryAsKB(15_000) // 15 MB
-            .withIterations(10)
-            .withParallelism(1)
-            .withSalt(salt.decodeBytes())
-            .withSecret(key.decodeBytes())
+            .Builder(ARGON2_id)
+            .withMemoryAsKB(ARGON_MEMORY_KIB) // 15 MB
+            .withIterations(ARGON_ITERATIONS)
+            .withParallelism(ARGON_PARALLELISM)
+            .withSalt(salt)
+            .withSecret(secret)
             .build()
 
 
-        val hash = ByteArray(32)
+        val hash = ByteArray(HASH_SIZE_BYTES)
         argonGenerator.init(argon2Parameters)
         argonGenerator.generateBytes(password.toCharArray(), hash)
 
-        // TODO generate salt and return Tuple<salt, hash>
-        // TODO salt must to be stored in auth_user
-        return Base64.encode(hash)
+        return HashingResult(
+            salt = Base64.encode(salt),
+            hash = Base64.encode(hash)
+        )
+    }
+
+    private fun nextSalt(): ByteArray {
+        val salt = ByteArray(SALT_SIZE_BYTES)
+        secureRandom.nextBytes(salt)
+        return salt
     }
 }
-
-fun main() { // TODO delete
-    val salt = Base64.of("afo5hZL7oBUPwm5PPGQwnw==") // 16 byte
-    val key = Base64.of("1ZzA+CxCCCVSxnsOaYdC4ZKkTvvx/oYw4ON0DwbNZFM=") // 32 byte
-    val hash = Argon2PasswordHasher(salt, key).hash("password")
-
-    println("salt: $salt")
-    println("key: $key")
-    println("hash: $hash")
-}
-
-
-
