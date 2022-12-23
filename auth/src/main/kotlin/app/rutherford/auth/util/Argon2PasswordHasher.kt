65ae99a30b7b9608040d5e5fb6a8a5987ef2da00
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-23 15:30:30 +0100

    add todo

--- auth/src/main/kotlin/app/rutherford/auth/util/Argon2PasswordHasher.kt
+++ auth/src/main/kotlin/app/rutherford/auth/util/Argon2PasswordHasher.kt
@@ -12,8 +12,8 @@ class Argon2PasswordHasher(private val salt: Base64, private val key: Base64) { 
         val argon2Parameters = Argon2Parameters
             // Recommended min by OWASP
             .Builder(Argon2Parameters.ARGON2_id)
-            .withMemoryAsKB(37_000) // 37 MB
-            .withIterations(1)
+            .withMemoryAsKB(37_000) // 37 MB // TODO use more memory?
+            .withIterations(1) // TODO use more iterations
             .withParallelism(1)
             .withSalt(salt.decodeBytes())
             .withSecret(key.decodeBytes())
@@ -30,7 +30,7 @@ class Argon2PasswordHasher(private val salt: Base64, private val key: Base64) { 
     }
 }
 
-fun main() {
+fun main() { // TODO delete
     val salt = Base64.of("afo5hZL7oBUPwm5PPGQwnw==") // 16 byte
     val key = Base64.of("1ZzA+CxCCCVSxnsOaYdC4ZKkTvvx/oYw4ON0DwbNZFM=") // 32 byte
     val hash = Argon2PasswordHasher(salt, key).hash("password")
