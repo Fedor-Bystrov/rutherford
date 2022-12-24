db7e5f5a144aba9c94fea619222b234ce35f2ead
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-24 17:16:47 +0100

    update V1 migration, use bytea, add salt fields

--- schema/src/main/resources/db/migration/V1__initial.sql
+++ schema/src/main/resources/db/migration/V1__initial.sql
@@ -6,7 +6,8 @@ CREATE TABLE auth_user (
     application_name VARCHAR(64) NOT NULL,
     email TEXT NOT NULL,
     email_confirmed BOOLEAN NOT NULL DEFAULT FALSE,
-    password_hash TEXT NOT NULL
+    salt BYTEA NOT NULL,
+    password_hash BYTEA NOT NULL
 );
 
 CREATE UNIQUE INDEX users_email_application_name_unique_idx ON auth_user (LOWER(email), application_name);
@@ -17,7 +18,8 @@ CREATE TABLE auth_user_token (
     updated_at TIMESTAMP NOT NULL,
     expiration TIMESTAMP,
     state VARCHAR(32) NOT NULL,
-    token_hash TEXT NOT NULL,
+    salt BYTEA NOT NULL,
+    token_hash BYTEA NOT NULL,
     user_id UUID REFERENCES auth_user (id) NOT NULL
 );
 
