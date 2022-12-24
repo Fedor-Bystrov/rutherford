20219eba55fa839424b8ca8514368b48cf5aed7d
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-24 17:50:24 +0100

    clean up Fixtures.kt

--- app/src/testFunctional/kotlin/app/rutherford/fixtures/Fixtures.kt
+++ app/src/testFunctional/kotlin/app/rutherford/fixtures/Fixtures.kt
@@ -7,7 +7,6 @@ import app.rutherford.core.abstract.entity.Entity.Id
 import app.rutherford.core.abstract.entity.Entity.State.CREATED
 import app.rutherford.core.types.Base64
 import org.apache.commons.lang3.RandomStringUtils.randomAlphabetic
-import org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric
 import org.apache.commons.lang3.RandomUtils.nextBytes
 import java.time.Instant.now
 import java.util.UUID.randomUUID
@@ -20,8 +19,8 @@ fun anAuthUser() = authUser()
     .applicationName(TEST1)
     .email("${randomAlphabetic(10)}@test.com")
     .emailConfirmed(false)
-    .salt(Base64.encode(nextBytes(16)))
-    .passwordHash(Base64.encode(nextBytes(32)))
+    .salt(randomBase64(16))
+    .passwordHash(randomBase64(32))
 
 fun anAuthUserToken() = authUserToken()
     .id(randomUUID())
@@ -29,5 +28,10 @@ fun anAuthUserToken() = authUserToken()
     .updatedAt(now())
     .expiration(now())
     .state(CREATED)
-    .tokenHash(randomAlphanumeric(50))
+    .tokenHash(randomBase64(32))
     .userId(Id(randomUUID()))
+
+
+fun randomBytes(count: Int): ByteArray = nextBytes(count)
+
+fun randomBase64(count: Int): Base64 = Base64.encode(randomBytes(count))
