e4ef779fcaf9d58622b8b9d26bca46668eb22e2c
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-25 19:33:53 +0100

    init UserManagerCreateTest

---
+++ app/src/testFunctional/kotlin/app/rutherford/auth/manager/usermanager/UserManagerCreateTest.kt
@@ -0,0 +1,31 @@
+package app.rutherford.auth.manager.usermanager
+
+import app.rutherford.FunctionalTest
+import org.junit.jupiter.api.Test
+
+class UserManagerCreateTest : FunctionalTest() {
+    @Test
+    fun `should throw when email is blank`() {
+        TODO("implement")
+    }
+
+    @Test
+    fun `should throw when password don't satisfy policy`() {
+        TODO("implement")
+    }
+
+    @Test
+    fun `should throw when email and application combination already exist`() {
+        TODO("implement")
+    }
+
+    @Test
+    fun `should create user`() {
+        TODO("implement")
+    }
+
+    @Test
+    fun `should create user with different application_name`() {
+        TODO("implement")
+    }
+}
