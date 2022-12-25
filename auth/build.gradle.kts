56684ee5001a1cdbdb09a9beb8d123fdf80f54e6
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-25 20:38:52 +0100

    add javalin dep to auth module

--- auth/build.gradle.kts
+++ auth/build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
     implementation(Dependencies.orgJson)
     implementation(Dependencies.passay)
     implementation(Dependencies.bcprov)
+    implementation(Dependencies.javalin)
 
     testImplementation(kotlin("test"))
     testImplementation(Dependencies.junitJupiter)
