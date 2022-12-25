6b561e839070b54433ecabe66240e04c45174eca
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-25 21:08:56 +0100

    add SignUpRequest

---
+++ auth/src/main/kotlin/app/rutherford/auth/resource/request/SignUpRequest.kt
@@ -0,0 +1,12 @@
+package app.rutherford.auth.resource.request
+
+import app.rutherford.core.util.Checks.validateEmailFormat
+import app.rutherford.core.util.Checks.validateNotBlank
+
+data class SignUpRequest(val email: String, val password1: String, val password2: String) {
+    init { // TODO test that validation returns correct exceptions
+        validateEmailFormat(email)
+        validateNotBlank("password1", email)
+        validateNotBlank("password2", email)
+    }
+}
