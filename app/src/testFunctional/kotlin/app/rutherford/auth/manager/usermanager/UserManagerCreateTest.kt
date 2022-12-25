f6085c55464dfb9897db7b2e0d1ea9daf2c82a93
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-25 20:03:24 +0100

    impl `should throw when email is blank` test

--- app/src/testFunctional/kotlin/app/rutherford/auth/manager/usermanager/UserManagerCreateTest.kt
+++ app/src/testFunctional/kotlin/app/rutherford/auth/manager/usermanager/UserManagerCreateTest.kt
@@ -1,12 +1,47 @@
 package app.rutherford.auth.manager.usermanager
 
 import app.rutherford.FunctionalTest
+import app.rutherford.auth.entity.AuthUser
+import app.rutherford.core.ApplicationName.TEST1
+import app.rutherford.core.transaction.transaction
+import app.rutherford.fixtures.anAuthUser
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class UserManagerCreateTest : FunctionalTest() {
-    @Test
-    fun `should throw when email is blank`() {
-        TODO("implement")
+
+    private lateinit var user: AuthUser
+
+    @BeforeEach
+    fun setUp() {
+        user = transaction {
+            authUserRepository.insert(
+                this, anAuthUser()
+                    .emailConfirmed(true)
+                    .applicationName(TEST1)
+                    .email("test@test.com")
+                    .build()
+            )
+        }!!
+    }
+
+    @ParameterizedTest
+    @ValueSource(
+        strings = [
+            "",
+            " ",
+            "   ",
+            "         "
+        ]
+    )
+    fun `should throw when email is blank`(email: String) {
+        // then
+        assertThatThrownBy { userManager.create(email, TEST1, "Passw0rd") }
+            .isInstanceOf(IllegalStateException::class.java)
+            .hasMessage("email is null or blank")
     }
 
     @Test
