f3406dcf7002bdc04ae3d388226245b46df0f2ff
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-26 14:24:35 +0100

    add ApplicationName#findForOrigin

--- core/src/main/kotlin/app/rutherford/core/ApplicationName.kt
+++ core/src/main/kotlin/app/rutherford/core/ApplicationName.kt
@@ -1,9 +1,17 @@
 package app.rutherford.core
 
-enum class ApplicationName {
+import java.net.URL
+
+enum class ApplicationName(private val allowedHost: URL) {
     /**
      * Only for tests
      */
-    TEST1, // TODO delete
-    TEST2,  // TODO delete
+    TEST1(URL("http://localhost:7070")), // TODO delete
+    TEST2(URL("http://localhost2:7070"));  // TODO delete
+
+    companion object {
+        fun findForOrigin(originURL: URL): ApplicationName? = ApplicationName
+            .values()
+            .find { it.allowedHost == originURL }
+    }
 }
