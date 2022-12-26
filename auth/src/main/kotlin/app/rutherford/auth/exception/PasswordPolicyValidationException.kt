f4343a19a15f87278a85be22c6f77c7e041935e1
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-26 12:54:33 +0100

    clean up PasswordPolicyValidationException

--- auth/src/main/kotlin/app/rutherford/auth/exception/PasswordPolicyValidationException.kt
+++ auth/src/main/kotlin/app/rutherford/auth/exception/PasswordPolicyValidationException.kt
@@ -1,7 +1,11 @@
 package app.rutherford.auth.exception
 
+import app.rutherford.core.ErrorCode
+import app.rutherford.core.ErrorCode.PASSWORD_POLICY_VIOLATION
+import app.rutherford.core.exception.RutherfordException
 import org.json.JSONObject
 
-class PasswordPolicyValidationException(private val errors: List<String>) : RuntimeException() {
-    fun toJson(): JSONObject = JSONObject().put("errors", errors)
+class PasswordPolicyValidationException(private val errors: List<String>) : RutherfordException() {
+    fun toJson(): JSONObject = JSONObject().put("errors", errors) // TODO delete
+    override fun errorCode(): ErrorCode = PASSWORD_POLICY_VIOLATION
 }
