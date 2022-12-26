b766aec6ded280129157c9f10d21b30d7eea9fc3
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-26 13:09:53 +0100

    clean up error handlers

--- app/src/main/kotlin/app/rutherford/module/JavalinModule.kt
+++ app/src/main/kotlin/app/rutherford/module/JavalinModule.kt
@@ -1,6 +1,9 @@
 package app.rutherford.module
 
+import app.rutherford.core.ErrorCode
+import app.rutherford.core.ErrorCode.MALFORMED_JSON
 import app.rutherford.core.exception.EntityNotFoundException
+import app.rutherford.core.exception.RutherfordException
 import app.rutherford.core.response.ErrorResponse
 import com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL
 import com.fasterxml.jackson.core.JsonParseException
@@ -63,10 +66,10 @@ class JavalinModule {
         JavalinValidation.register(UUID::class.java) { UUID.fromString(it) }
 
         // Exception Mappers // TODO write tests on mappers
-        exception(JsonParseException::class) { e, c -> nonCritical(e, c, BAD_REQUEST, "Malformed JSON") }
-        exception(ValidationException::class) { e, c -> nonCritical(e, c, BAD_REQUEST, getMessage(e)) }
-        exception(IllegalStateException::class) { e, c -> nonCritical(e, c, BAD_REQUEST, e.message) }
-        exception(EntityNotFoundException::class) { e, c -> nonCritical(e, c, NOT_FOUND, "Not Found") }
+        exception(JsonParseException::class) { e, c -> nonCritical(e, c, BAD_REQUEST, MALFORMED_JSON) }
+        exception(ValidationException::class) { e, c -> nonCritical(e, c, BAD_REQUEST) } // TODO return details
+        exception(IllegalStateException::class) { e, c -> nonCritical(e, c, BAD_REQUEST) } // TODO return details
+        exception(EntityNotFoundException::class) { e, c -> nonCritical(e, c, NOT_FOUND) }
         exception(Exception::class) { e, c -> internalServerError(e, c) }
     }
 
@@ -81,15 +84,30 @@ class JavalinModule {
     private fun <E : Exception> exception(clazz: KClass<E>, exceptionHandler: ExceptionHandler<E>) =
         javalin.exception(clazz.java, exceptionHandler)
 
-    private fun <E : Exception> nonCritical(e: E, ctx: Context, httpStatus: HttpStatus, message: String?) {
+    private fun <E : Exception> nonCritical(
+        e: E,
+        ctx: Context,
+        httpStatus: HttpStatus,
+        errorCode: ErrorCode? = null
+    ) {
         logger.info("An error occurred", e)
         ctx.status(httpStatus)
-        ctx.json(
-            ErrorResponse(
-                message = message,
-                httpStatus = httpStatus.code
+
+        if (e is RutherfordException) {
+            ctx.json(
+                ErrorResponse(
+                    httpStatus = httpStatus.code,
+                    code = e.errorCode(),
+                )
             )
-        )
+        } else {
+            ctx.json(
+                ErrorResponse(
+                    httpStatus = httpStatus.code,
+                    code = errorCode
+                )
+            )
+        }
     }
 
     private fun <E : Exception> internalServerError(e: E, ctx: Context) {
@@ -97,8 +115,8 @@ class JavalinModule {
         ctx.status(INTERNAL_SERVER_ERROR)
         ctx.json(
             ErrorResponse(
-                message = INTERNAL_SERVER_ERROR.name,
-                httpStatus = INTERNAL_SERVER_ERROR.code
+                code = ErrorCode.INTERNAL_SERVER_ERROR,
+                httpStatus = INTERNAL_SERVER_ERROR.code,
             )
         )
 
--- core/src/main/kotlin/app/rutherford/core/ErrorCode.kt
+++ core/src/main/kotlin/app/rutherford/core/ErrorCode.kt
@@ -1,6 +1,8 @@
 package app.rutherford.core
 
 enum class ErrorCode {
+    INTERNAL_SERVER_ERROR,
+    MALFORMED_JSON,
     ENTITY_NOT_FOUND,
     USER_ALREADY_EXISTS,
     PASSWORD_POLICY_VIOLATION
