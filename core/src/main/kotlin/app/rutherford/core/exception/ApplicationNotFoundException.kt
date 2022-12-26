637e47c50ce9496a0982c2b12ed71ea4f65bd319
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-26 14:46:49 +0100

    clean up ApplicationNotFoundException

--- core/src/main/kotlin/app/rutherford/core/exception/ApplicationNotFoundException.kt
+++ core/src/main/kotlin/app/rutherford/core/exception/ApplicationNotFoundException.kt
@@ -5,5 +5,5 @@ import app.rutherford.core.ErrorCode.APPLICATION_NOT_FOUND
 
 class ApplicationNotFoundException(originUrl: String?) :
     RutherfordException("No application registered for $originUrl") {
-    override fun errorCode(): ErrorCode = APPLICATION_NOT_FOUND;
+    override fun errorCode(): ErrorCode = APPLICATION_NOT_FOUND
 }
