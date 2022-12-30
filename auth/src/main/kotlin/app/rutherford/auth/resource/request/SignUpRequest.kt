17c2a4d7ac59475e404c963783405fb951db02ca
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-30 21:36:25 +0100

    clean up SignUpRequest

--- auth/src/main/kotlin/app/rutherford/auth/resource/request/SignUpRequest.kt
+++ auth/src/main/kotlin/app/rutherford/auth/resource/request/SignUpRequest.kt
@@ -4,10 +4,10 @@ import app.rutherford.core.util.Checks.validateEmailFormat
 import app.rutherford.core.util.Checks.validateNotBlank
 
 data class SignUpRequest(val email: String, val password1: String, val password2: String) {
-    init { // TODO test that validation returns correct exceptions
+    init {
         validateEmailFormat(email)
         validateNotBlank("password1", email)
         validateNotBlank("password2", email)
-        check(password1 == password2) { "PASSWORDS_MISMATCH" } // TODO write test + check response is correct
+        check(password1 == password2) { "PASSWORDS_MISMATCH" }
     }
 }
