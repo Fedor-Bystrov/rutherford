482b3ec6fd596a02026b7bbeb74a62192109daab
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-30 21:48:24 +0100

    init AuthResourceSignUpTest

---
+++ app/src/testFunctional/kotlin/app/rutherford/auth/resource/AuthResourceSignUpTest.kt
@@ -0,0 +1,48 @@
+package app.rutherford.auth.resource
+
+import app.rutherford.FunctionalTest
+import org.junit.jupiter.api.Test
+
+class AuthResourceSignUpTest : FunctionalTest() {
+    @Test
+    fun `should validate email is not null`() {
+        TODO("impl")
+    }
+
+    @Test
+    fun `should validate email is not blank`() {
+        TODO("impl")
+    }
+
+    @Test
+    fun `should validate email format`() {
+        TODO("impl")
+    }
+
+    @Test
+    fun `should validate password1 is not null`() {
+        TODO("impl")
+    }
+
+    @Test
+    fun `should validate password1 is not blank`() {
+        TODO("impl")
+    }
+
+    @Test
+    fun `should validate password2 is not null`() {
+        TODO("impl")
+    }
+
+    @Test
+    fun `should validate password2 is not blank`() {
+        TODO("impl")
+    }
+
+    @Test
+    fun `should validate passwords match`() {
+        TODO("impl")
+    }
+
+    // TODO add more tests
+}
