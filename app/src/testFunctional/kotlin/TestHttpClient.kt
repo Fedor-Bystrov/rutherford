4ad8fb5efcd7072621af8b38cc1f56dc0c64df5b
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-30 22:26:30 +0100

    impl TestHttpClient#post

--- app/src/testFunctional/kotlin/TestHttpClient.kt
+++ app/src/testFunctional/kotlin/TestHttpClient.kt
@@ -1,18 +1,32 @@
 import org.json.JSONObject
 import java.net.URI
 import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpRequest.BodyPublishers
+import java.net.http.HttpResponse
+import java.net.http.HttpResponse.BodyHandlers
 import java.time.Duration
 
-const val HOST_URI = "http://localhost"
-
 class TestHttpClient(applicationPort: Int) {
-    private val baseURI = URI.create("$HOST_URI:$applicationPort")
+    private val baseURI = "http://localhost:$applicationPort"
 
-    private val http = HttpClient.newBuilder()
+    private val httpClient = HttpClient.newBuilder()
         .connectTimeout(Duration.ofMinutes(1))
         .build()
 
-    fun post(uri: String, body: JSONObject? = null): JSONObject? {
-        return null // TODO
+    fun post(uri: String, body: JSONObject? = null): HttpResponse<String> {
+        val request = newRequest()
+            .uri(URI.create(baseURI + uri))
+            .POST(
+                if (body == null) BodyPublishers.noBody()
+                else BodyPublishers.ofString(body.toString(1))
+            )
+            .build()
+
+        return httpClient.send(request, BodyHandlers.ofString())
     }
+
+    private fun newRequest(): HttpRequest.Builder = HttpRequest
+        .newBuilder()
+        .timeout(Duration.ofMinutes(1))
 }
