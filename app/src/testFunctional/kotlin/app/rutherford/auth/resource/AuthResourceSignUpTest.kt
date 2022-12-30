f52a93162396c31a110453ef145d01501f15e2f7
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-30 22:32:59 +0100

    add todos to AuthResourceSignUpTest

--- app/src/testFunctional/kotlin/app/rutherford/auth/resource/AuthResourceSignUpTest.kt
+++ app/src/testFunctional/kotlin/app/rutherford/auth/resource/AuthResourceSignUpTest.kt
@@ -1,9 +1,22 @@
 package app.rutherford.auth.resource
 
 import app.rutherford.FunctionalTest
+import io.javalin.http.HttpStatus.BAD_REQUEST
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class AuthResourceSignUpTest : FunctionalTest() {
+    @Test
+    fun `should returned MALFORMED_JSON when body is empty`() {
+        // given
+        // when
+        val response = http.post("/api/auth/sign-up")
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(BAD_REQUEST.code)
+        // TODO assertJsonEquals
+    }
+
     @Test
     fun `should validate email is not null`() {
         TODO("impl")
