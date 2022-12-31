0194e8b2931e9ea49eef6808f73e60f83db4382a
Fedor Bystrov <augustbystrov@gmail.com>
2022-12-31 14:21:19 +0100

    add DESERIALIZATION_FAILED on null and empty body tests

--- app/src/testFunctional/kotlin/app/rutherford/auth/resource/AuthResourceSignUpTest.kt
+++ app/src/testFunctional/kotlin/app/rutherford/auth/resource/AuthResourceSignUpTest.kt
@@ -3,18 +3,47 @@ package app.rutherford.auth.resource
 import app.rutherford.FunctionalTest
 import io.javalin.http.HttpStatus.BAD_REQUEST
 import org.assertj.core.api.Assertions.assertThat
+import org.json.JSONObject
 import org.junit.jupiter.api.Test
+import org.skyscreamer.jsonassert.JSONAssert.assertEquals as assertJsonEquals
 
 class AuthResourceSignUpTest : FunctionalTest() {
     @Test
-    fun `should returned MALFORMED_JSON when body is empty`() {
-        // given
+    fun `should returned DESERIALIZATION_FAILED when body is null`() {
         // when
         val response = http.post("/api/auth/sign-up")
 
         // then
         assertThat(response.statusCode()).isEqualTo(BAD_REQUEST.code)
-        // TODO assertJsonEquals
+        assertJsonEquals(
+            """{
+                    "httpStatus": ${BAD_REQUEST.code},
+                    "code": "VALIDATION_ERROR",
+                    "errors": [
+                        "DESERIALIZATION_FAILED"
+                    ]
+                }""",
+            response.body(), true
+        )
+    }
+
+    @Test
+    fun `should returned DESERIALIZATION_FAILED when body is empty`() {
+        // when
+        val response = http.post("/api/auth/sign-up", JSONObject())
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(BAD_REQUEST.code)
+        assertJsonEquals(
+            """{
+                    "httpStatus": ${BAD_REQUEST.code},
+                    "code": "VALIDATION_ERROR",
+                    "errors": [
+                        "DESERIALIZATION_FAILED"
+                    ]
+                }""",
+            response.body(), true
+        )
     }
 
     @Test
