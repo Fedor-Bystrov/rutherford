70270c979f77242f23381479140621174c5daa4b
Fedor Bystrov <augustbystrov@gmail.com>
2023-01-07 18:54:53 +0100

    set up protobuf and grpc generaiton

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,11 @@
+import com.google.protobuf.gradle.id
 import java.time.Instant.now
 
 plugins {
     kotlin("jvm") version Dependencies.Plugins.Versions.kotlin
     id("org.unbroken-dome.test-sets") version Dependencies.Plugins.Versions.testSets
     id("com.google.cloud.tools.jib") version Dependencies.Plugins.Versions.jib
+    id("com.google.protobuf") version "0.9.1" // TODO add to Dependencies.Plugins.Versions
     application
 }
 
@@ -24,6 +26,12 @@ dependencies {
     implementation(Dependencies.postgresql)
     implementation(Dependencies.jooq)
     implementation(Dependencies.hikari)
+    // TODO add to Dependencies
+    implementation("io.grpc:grpc-netty:1.51.1")
+    implementation("io.grpc:grpc-protobuf:1.51.1")
+    implementation("io.grpc:grpc-stub:1.51.1")
+    implementation("io.grpc:grpc-kotlin-stub:1.3.0")
+    implementation("com.google.protobuf:protobuf-kotlin:3.21.12")
 
     testImplementation(kotlin("test"))
     testImplementation(project(":core"))
@@ -47,6 +55,32 @@ application {
     mainClass.set("app.rutherford.MainKt")
     applicationDefaultJvmArgs = rutherfordJvmArgs
 }
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.21.12"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.51.1"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:1.3.0:jdk8@jar"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+            it.builtins {
+                id("kotlin")
+            }
+        }
+    }
+}
+
 
 jib {
     from {
