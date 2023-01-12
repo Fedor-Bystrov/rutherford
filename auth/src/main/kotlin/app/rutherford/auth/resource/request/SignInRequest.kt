a909910f3840579b20f91fa8a9c3ea72f255ad8a
Fedor Bystrov <augustbystrov@gmail.com>
2023-01-12 18:59:06 +0100

    add SignInRequest

--- auth/src/main/kotlin/app/rutherford/auth/resource/request/SignInRequest.kt
+++ auth/src/main/kotlin/app/rutherford/auth/resource/request/SignInRequest.kt
@@ -1,9 +1,8 @@
 package app.rutherford.auth.resource.request
 
-data class SignUpRequest(
+data class SignInRequest(
     val email: String,
-    val password1: String,
-    val password2: String
+    val password: String,
 ) {
     override fun toString(): String {
         return "<masked>"
