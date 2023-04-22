123755d5c2af4a1ce568d82217f4c31e8fd3c339
Fedor Bystrov <augustbystrov@gmail.com>
2023-04-22 13:48:29 +0100

    delete AuthUserTokenRepository.kt

--- app/src/main/kotlin/app/rutherford/module/RepositoryModule.kt
+++ app/src/main/kotlin/app/rutherford/module/RepositoryModule.kt
@@ -1,15 +1,12 @@
 package app.rutherford.module
 
 import app.rutherford.auth.repository.AuthUserRepository
-import app.rutherford.auth.repository.AuthUserTokenRepository
 import org.jooq.DSLContext
 
 class RepositoryModule(dslContext: DSLContext) {
     val authUserRepository: AuthUserRepository
-    val authUserTokenRepository: AuthUserTokenRepository
 
     init {
         authUserRepository = AuthUserRepository(dslContext)
-        authUserTokenRepository = AuthUserTokenRepository(dslContext)
     }
 }
